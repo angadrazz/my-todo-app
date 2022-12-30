c4ec3e29530254048a5e1d32a0792ab236d93f6f
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-30 23:45:52 +0530

    app .js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+function ToDoList() {
+  const [todo, settodo] = useState([]);
+
+  const submitTask = (e) => {
+    e.preventDefault();
+
+    let task = {
+      title: e.target.elements.title.value,
+      description: e.target.elements.description.value,
+      completed: false,
+    };
+    settodo([...todo, task]);
+  };
+  const markAsCompleted = (title) => {
+    settodo((prevState) => {
+      let updatedTasks = prevState.map((task) => {
+        if (task.title === title) {
+          return {
+            ...task,
+            completed: true,
+          };
+        }
+        return task;
+      });
+      return updatedTasks;
+    });
+  };
+  const deleteTask = (title) => {
+    settodo((prevState) => prevState.filter((task) => task.title !== title));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" onSubmit={submitTask}>
+
+          <label htmlFor="" >Title</label>
+          <input type="text" name="title"/>
+
+          <label htmlFor="">Description</label>
+          <input type="text" name="description"/>
+
+        <input type="submit" value="Add Task" />
+      </form>
+      <ul>
+      {todo.map((task) => (
+        <li key={task.title}>
+          {task.title}: {task.description}
+          {task.completed ? <span> (Task completed) </span> : <span> (Task not completed) </span>}
+          <button onClick={() => markAsCompleted(task.title)}>Mark as completed</button>
+           <button onClick={() => deleteTask(task.title)}>Delete</button>
+        </li>
+      ))}
+    </ul>
     </div>
   );
 }
 
-export default App;
+export default ToDoList;
